73c5272d97207db40da038db111dda14750a27f0
Brian Philip <bp@north-net.co.uk>
2015-08-28 19:08:26 +0100

    Various script changes.

--- post-ks-config.bash
+++ post-ks-config.bash
@@ -4,7 +4,7 @@ cd /tmp/auto_build/
 /usr/bin/wget http://192.168.2.1/pub/auto_build/vmhost_autobuild.csv > /dev/null 2&>1
 DFEAULT_INT=`/sbin/route -n | /usr/bin/grep "^0.0.0.0" | /usr/bin/awk '{ print $8 }'`
 MAC_ADD=`/sbin/ifconfig ${DFEAULT_INT} | /usr/bin/grep ether | /usr/bin/awk '{ print $2 }'`
-MY_IP=`/sbin/ifconfig eno16777728 | /usr/bin/grep inet | /usr/bin/grep -v inet6 | /usr/bin/awk ' {print $2} '`
+MY_IP=`/sbin/ifconfig ${DFEAULT_INT} | /usr/bin/grep inet | /usr/bin/grep -v inet6 | /usr/bin/awk ' {print $2} '`
 MY_NAME=`/usr/bin/grep "${MAC_ADD}" /tmp/auto_build/vmhost_autobuild.csv | /usr/bin/tail -1 | /usr/bin/cut -f 2 -d , | /usr/bin/tr '[A-Z]' '[a-z]'`
 /usr/bin/hostnamectl set-hostname ${MY_NAME}
 /usr/bin/echo "${MY_NAME}" > /etc/hostname
